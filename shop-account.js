eb0ef72b3e27caafdd73fb9f1317c4f033716d90
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-01-01 19:37:06 +0100

    Update shop-account.js

--- shop-account.js
+++ shop-account.js
@@ -3,21 +3,25 @@ $(document).ready(function(){
 				$("#account-view").hide();
 				$("#shop").click(function(){
 					if($("#shop-view").is(":hidden")){
-						$("#shop-view").show('slow');
-						$("#account-view").hide('slow');
+						$("#shop-view").show('fast');
+						$("#account-view").hide('fast');
 					}
 					else if($("#shop-view").is(":visible")){
-						$("#shop-view").hide('slow');
+						$("#shop-view").hide('fast');
 					}
 				});
 
 				$("#account").click(function(){
 					if($("#account-view").is(":hidden")){
-						$("#account-view").show('slow');
-						$("#shop-view").hide('slow');
+						$("#account-view").show('fast');
+						$("#shop-view").hide('fast');
 					}
 					else if($("#account-view").is(":visible")){
-						$("#account-view").hide('slow');
+						$("#account-view").hide('fast');
 					}
+				})
+
+				$("#blej").click(function(){
+					$("#shop-view").show('fast');
 				})
 			});
