e9e12ae1220133a7057108cae18346103e335603
Margarita-N <58574357+Margarita-N@users.noreply.github.com>
2020-01-01 19:27:43 +0100

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,27 @@
+function resetCount()
+{
+  localStorage.clickcount=0;
+}
+function clickCounter()
+{
+if(typeof(Storage)!=="undefined")
+{
+    if (localStorage.clickcount)
+    {
+    localStorage.clickcount=Number(localStorage.clickcount)+1;
+    }
+  else
+    {
+    localStorage.clickcount=1;
+    }
+  document.getElementById("result").innerHTML="Numri i librave ne shporte: " + localStorage.clickcount;
+  }
+else
+  {
+  document.getElementById("result").innerHTML="Sorry, your browser does not support web storage...";
+  }
+}
+function paid()
+{
+  document.getElementById("pay").innerHTML="PAID!";
+}
